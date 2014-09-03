4ccc4cd665be13d00246ac2a7251bcf6b79b77a6
ingsis603 <aleju9306@gmail.com>
2014-09-03 16:12:41 -0500

    parqueadero nuevo

---
+++ ParqueaderoVS/Parqueadero/Usuarios/Administrativo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParqueaderoVS.Parqueadero.Usuarios
+{
+    public class Administrativo
+    {
+    }
+}
